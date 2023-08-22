d4cb54c675c0973aa2ffd01213dc07b4ce6e2e6b
Marvin Lee <marvinlee.github@gmail.com>
2023-08-21 22:37:45 -0700

    Fix pathing for routerLinks

--- src/app/config/constants.service.ts
+++ src/app/config/constants.service.ts
@@ -5,14 +5,14 @@ import { environment } from "../../environments/environment";
 	providedIn: "root"
 })
 export class ConstantsService {
-	SERVER_URL: string = environment.SERVER_URL;
+	public SERVER_URL: string = environment.SERVER_URL;
+
 	constructor() {
 		if(!this.SERVER_URL) {
 			throw new Error(`SERVER_URL not configured: ${this.SERVER_URL}`);
 		}
 		if(this.SERVER_URL.endsWith("/")) {
-			this.SERVER_URL = this.SERVER_URL.slice(0, this.SERVER_URL.length - 1);
-			console.log(this.SERVER_URL)
+			this.SERVER_URL = this.SERVER_URL.slice(0, -1);
 		}
 	}
 }
