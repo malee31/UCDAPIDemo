f6ca65f21f0a596973c0c0d6808f108905167be9
Marvin Lee <marvinlee.github@gmail.com>
2023-09-17 15:29:09 -0700

    Generated blank pages and a coming soon banner for all the placeholder pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,9 @@ import { LogsComponent } from "./logs/logs.component";
 import { ChartSearchComponent } from "./chart-search/chartSearch.component";
 import { ChartComponent } from "./chart/chart.component";
 import { MultiChartComponent } from "./multichart/multi-chart.component";
+import { CoursesComponent } from "./courses/courses.component";
+import { DegreesComponent } from "./degrees/degrees.component";
+import { SeatsComponent } from "./seats/seats.component";
 
 const routes: Routes = [
   { path: "home", component: HomeComponent },
@@ -13,6 +16,9 @@ const routes: Routes = [
 	{ path: "chart", component: ChartSearchComponent },
 	{ path: "chart/crn/:crn", component: ChartComponent },
 	{ path: "chart/name/:subject_code/:subject_number", component: MultiChartComponent },
+	{ path: "degrees", component: DegreesComponent },
+	{ path: "courses", component: CoursesComponent },
+	{ path: "seats", component: SeatsComponent },
   { path: "", redirectTo: "home", pathMatch: "full" },
   { path: "**", component: HTTP404Component },
 ];
