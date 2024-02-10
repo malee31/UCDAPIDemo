9694e07f9f2ce2b81228072d352a7cae262f9570
Marvin Lee <marvinlee.github@gmail.com>
2024-02-10 12:04:16 -0800

    Added safe padding areas for PWA while in fullscreen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -136,12 +136,12 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 		AsyncPipe,
 		MatProgressSpinnerModule,
 		NgOptimizedImage,
-  ServiceWorkerModule.register('ngsw-worker.js', {
-    enabled: !isDevMode(),
-    // Register the ServiceWorker as soon as the application is stable
-    // or after 30 seconds (whichever comes first).
-    registrationStrategy: 'registerWhenStable:30000'
-  }),
+		ServiceWorkerModule.register('ngsw-worker.js', {
+			enabled: !isDevMode(),
+			// Register the ServiceWorker as soon as the application is stable
+			// or after 30 seconds (whichever comes first).
+			registrationStrategy: 'registerWhenStable:30000'
+		}),
 	],
 	providers: [ConstantsService, ApiService],
 	bootstrap: [AppComponent]
